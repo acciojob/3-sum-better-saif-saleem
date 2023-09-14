a6544d2aac3f860f2f6df8bfe49442d652a96d70
saif-saleem <133539527+saif-saleem@users.noreply.github.com>
2023-09-15 00:04:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 function threeSum(arr, target) {
-// write your code here
-	let d=Number. MAX_VALUE;
-	for(let i=0;i<arr.length-2;i++){
-		
-		for (let j = 0; j < arr.length-1; j++) {
-			for (let k = 0; k < arr.length; k++) {
-				let s=arr[i]+arr[j]+arr[k];
-				 if (Math.abs(s - target) < Math.abs(d - target)) {
-          d = s;
+  let closestSum = Number.MAX_VALUE;
+
+  for (let i = 0; i < arr.length - 2; i++) {
+    for (let j = i + 1; j < arr.length - 1; j++) {
+      for (let k = j + 1; k < arr.length; k++) {
+        let s = arr[i] + arr[j] + arr[k];
+        
+        // Compare the absolute difference with the target
+        if (Math.abs(s - target) < Math.abs(closestSum - target)) {
+          closestSum = s;
         }
-			}
-			
-		}
-		return d;
-	}
-  
+      }
+    }
+  }
+
+  return closestSum;
 }
 
 module.exports = threeSum;
