121beaed209a2c167b6b54035db4f649bc1042c4
saif-saleem <133539527+saif-saleem@users.noreply.github.com>
2023-09-15 00:01:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,20 +3,12 @@ function threeSum(arr, target) {
 	let d=Number. MAX_VALUE;
 	for(let i=0;i<arr.length-2;i++){
 		
-		for (let j = 0; j < arr.lenght-1; j++) {
+		for (let j = 0; j < arr.length-1; j++) {
 			for (let k = 0; k < arr.length; k++) {
 				let s=arr[i]+arr[j]+arr[k];
-				if(s>target){
-					if (s-target<d) {
-						
-					d=s;
-					}
-				}
-				else{
-					if(target-s<d){
-						d=s;
-					}
-				}
+				 if (Math.abs(s - target) < Math.abs(d - target)) {
+          d = s;
+        }
 			}
 			
 		}
